b04e78fc5f724277ada7f9dd76b08c6c00fb5d34
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2025-08-01 21:01:00 +0400

    :recycle: Moves Movement behavior

--- src/demo/movement.rs
+++ src/demo/movement.rs
@@ -1,83 +1,76 @@
-//! Handle player input and translate it into movement through a character
-//! controller. A character controller is the collection of systems that govern
-//! the movement of characters.
-//!
-//! In our case, the character controller has the following logic:
-//! - Set [`MovementController`] intent based on directional keyboard input.
-//!   This is done in the `player` module, as it is specific to the player
-//!   character.
-//! - Apply movement based on [`MovementController`] intent and maximum speed.
-//! - Wrap the character within the window.
-//!
-//! Note that the implementation used here is limited for demonstration
-//! purposes. If you want to move the player in a smoother way,
-//! consider using a [fixed timestep](https://github.com/bevyengine/bevy/blob/main/examples/movement/physics_in_fixed_timestep.rs).
+use std::f32::consts::TAU;
 
-use bevy::{prelude::*, window::PrimaryWindow};
-
-use crate::{AppSystems, PausableSystems};
+use bevy::prelude::*;
+use bevy_enhanced_input::prelude::*;
+use bevy_tnua::prelude::{TnuaBuiltinWalk, TnuaController};
 
 pub(super) fn plugin(app: &mut App) {
-    app.register_type::<MovementController>();
-    app.register_type::<ScreenWrap>();
+    app.add_systems(Update, apply_movement);
 
-    app.add_systems(
-        Update,
-        (apply_movement, apply_screen_wrap)
-            .chain()
-            .in_set(AppSystems::Update)
-            .in_set(PausableSystems),
-    );
-}
+    app.add_input_context::<DefaultInputContext>();
 
-/// These are the movement parameters for our character controller.
-/// For now, this is only used for a single player, but it could power NPCs or
-/// other players as well.
-#[derive(Component, Reflect)]
-#[reflect(Component)]
-pub struct MovementController {
-    /// The direction the character wants to move in.
-    pub intent: Vec2,
-
-    /// Maximum speed in world units per second.
-    /// 1 world unit = 1 pixel when using the default 2D camera and no physics engine.
-    pub max_speed: f32,
-}
-
-impl Default for MovementController {
-    fn default() -> Self {
-        Self {
-            intent: Vec2::ZERO,
-            // 400 pixels per second is a nice default, but we can still vary this per character.
-            max_speed: 400.0,
-        }
-    }
+    app.add_observer(apply_default_binding);
+    app.add_observer(remove_default_binding);
 }
 
 fn apply_movement(
-    time: Res<Time>,
-    mut movement_query: Query<(&mut MovementController, &mut Transform)>,
+    mut controller: Single<&mut TnuaController>,
+    transform: Single<&Transform, With<Camera3d>>,
+    move_action: Single<&Action<Move>, Changed<Action<Move>>>,
 ) {
-    for (mut controller, mut transform) in &mut movement_query {
-        let velocity = controller.max_speed * controller.intent;
-        controller.intent = Vec2::ZERO;
-        transform.translation += velocity.extend(0.0) * time.delta_secs();
+    let yaw = transform.rotation.to_euler(EulerRot::YXZ).0;
+    let yaw_quat = Quat::from_axis_angle(Vec3::Y, yaw);
+
+    controller.basis(TnuaBuiltinWalk {
+        desired_velocity: yaw_quat * ***move_action,
+        float_height: 2.0,
+        max_slope: TAU / 8.0,
+        ..default()
+    });
+}
+
+#[derive(Debug, InputAction)]
+#[action_output(Vec3)]
+pub(super) struct Move;
+
+#[derive(Debug, InputAction)]
+#[action_output(Vec2)]
+pub(super) struct Look;
+
+#[derive(Component)]
+pub(super) struct DefaultInputContext;
+
+impl DefaultInputContext {
+    fn bindings() -> impl Bundle {
+        actions!(
+            DefaultInputContext[(
+                Action::<Move>::new(),
+                DeadZone::default(),
+                SmoothNudge::default(),
+                Bindings::spawn((
+                    Cardinal::wasd_keys(),
+                    Axial::left_stick()
+                )),
+                Negate::y(),
+                SwizzleAxis::XZY,
+                Scale::splat(8.0)
+            ),
+            (
+                Action::<Look>::new(),
+                Bindings::spawn(Spawn((Binding::mouse_motion(), Negate::all(), SwizzleAxis::YXZ)))
+            )]
+        )
     }
 }
 
-#[derive(Component, Reflect)]
-#[reflect(Component)]
-pub struct ScreenWrap;
+fn apply_default_binding(trigger: Trigger<OnAdd, DefaultInputContext>, mut commands: Commands) {
+    commands
+        .entity(trigger.target())
+        .insert(DefaultInputContext::bindings());
+}
 
-fn apply_screen_wrap(
-    window: Single<&Window, With<PrimaryWindow>>,
-    mut wrap_query: Query<&mut Transform, With<ScreenWrap>>,
-) {
-    let size = window.size() + 256.0;
-    let half_size = size / 2.0;
-    for mut transform in &mut wrap_query {
-        let position = transform.translation.xy();
-        let wrapped = (position + half_size).rem_euclid(size) - half_size;
-        transform.translation = wrapped.extend(transform.translation.z);
-    }
+fn remove_default_binding(trigger: Trigger<OnRemove, DefaultInputContext>, mut commands: Commands) {
+    commands
+        .entity(trigger.target())
+        .despawn_related::<Actions<DefaultInputContext>>();
 }
--- src/demo/player.rs
+++ src/demo/player.rs
@@ -1,40 +1,19 @@
 //! Player-specific behavior.
 
-use std::f32::consts::TAU;
-
-use bevy::{
-    image::{ImageLoaderSettings, ImageSampler},
-    prelude::*,
-};
+use bevy::prelude::*;
 
 use bevy_enhanced_input::prelude::*;
-use bevy_tnua::prelude::{TnuaBuiltinWalk, TnuaController};
-
-use crate::asset_tracking::LoadResource;
+use bevy_tnua::prelude::TnuaController;
 
 use avian3d::prelude::*;
 
 pub(super) fn plugin(app: &mut App) {
     app.register_type::<Player>();
-    app.add_input_context::<DefaultInputContext>();
-
-    app.register_type::<PlayerAssets>();
-    app.load_resource::<PlayerAssets>();
-
-    println!("Adding player observer");
 
     app.add_observer(setup_player);
-    app.add_observer(apply_default_binding);
-    app.add_observer(remove_default_binding);
-
     app.add_observer(handled_player_looking);
 
-    app.add_systems(
-        Update,
-        (apply_movement, sync_player_camera.after(apply_movement)),
-    );
-
-    // Record directional input as movement controls.
+    app.add_systems(Update, sync_player_camera);
 }
 
 /// The player character.
@@ -46,7 +25,7 @@ pub fn player() -> impl Bundle {
 fn setup_player(trigger: Trigger<OnAdd, Player>, mut commands: Commands) {
     commands.entity(trigger.target()).insert((
         Name::new("Player"),
-        DefaultInputContext,
+        super::movement::DefaultInputContext,
         Transform::default(),
         RigidBody::Dynamic,
         Collider::sphere(0.5),
@@ -54,53 +33,16 @@ fn setup_player(trigger: Trigger<OnAdd, Player>, mut commands: Commands) {
     ));
 }
 
-fn apply_default_binding(trigger: Trigger<OnAdd, DefaultInputContext>, mut commands: Commands) {
-    commands
-        .entity(trigger.target())
-        .insert(default_input_context());
-}
-
-fn remove_default_binding(trigger: Trigger<OnRemove, DefaultInputContext>, mut commands: Commands) {
-    commands
-        .entity(trigger.target())
-        .despawn_related::<Actions<DefaultInputContext>>();
-}
-
-fn default_input_context() -> impl Bundle {
-    actions!(
-        DefaultInputContext[(
-            Action::<Move>::new(),
-            DeadZone::default(),
-            SmoothNudge::default(),
-            Bindings::spawn((
-                Cardinal::wasd_keys(),
-                Axial::left_stick()
-            )),
-            Negate::y(),
-            SwizzleAxis::XZY,
-            Scale::splat(8.0)
-        ),
-        (
-            Action::<Look>::new(),
-            Bindings::spawn(Spawn((Binding::mouse_motion(), Negate::all(), SwizzleAxis::YXZ)))
-        )]
-    )
-}
-
 #[derive(Component, Debug, Clone, Copy, PartialEq, Default, Reflect)]
 #[reflect(Component)]
 struct Player;
 
-#[derive(Component)]
-struct DefaultInputContext;
-
 fn handled_player_looking(
-    trigger: Trigger<Fired<Look>>,
+    trigger: Trigger<Fired<super::movement::Look>>,
     mut camera: Single<&mut Transform, With<Camera3d>>,
     time: Res<Time>,
     window: Single<&Window, With<bevy::window::PrimaryWindow>>,
 ) {
-    println!("looking: {}", trigger.value);
     if !window.focused {
         return;
     }
@@ -119,56 +61,3 @@ fn sync_player_camera(
 ) {
     camera.translation = player.translation;
 }
-
-fn apply_movement(
-    mut controller: Single<&mut TnuaController>,
-    transform: Single<&Transform, With<Camera3d>>,
-    move_action: Single<&Action<Move>, Changed<Action<Move>>>,
-) {
-    let yaw = transform.rotation.to_euler(EulerRot::YXZ).0;
-    let yaw_quat = Quat::from_axis_angle(Vec3::Y, yaw);
-    println!("desired_velocity: {:?}", yaw_quat * ***move_action);
-    controller.basis(TnuaBuiltinWalk {
-        // The `desired_velocity` determines how the character will move.
-        desired_velocity: yaw_quat * ***move_action,
-        // The `float_height` must be greater (even if by little) from the distance between the
-        // character's center and the lowest point of its collider.
-        float_height: 2.0,
-        // Restrict the max slope so that the player cannot walk up slightly angled chairs.
-        max_slope: TAU / 8.0,
-        ..default()
-    });
-}
-
-#[derive(Resource, Asset, Clone, Reflect)]
-#[reflect(Resource)]
-pub struct PlayerAssets {
-    #[dependency]
-    ducky: Handle<Image>,
-    #[dependency]
-    pub steps: Vec<Handle<AudioSource>>,
-}
-
-impl FromWorld for PlayerAssets {
-    fn from_world(world: &mut World) -> Self {
-        let assets = world.resource::<AssetServer>();
-        Self {
-            ducky: assets.load_with_settings(
-                "images/ducky.png",
-                |settings: &mut ImageLoaderSettings| {
-                    // Use `nearest` image sampling to preserve pixel art style.
-                    settings.sampler = ImageSampler::nearest();
-                },
-            ),
-            steps: vec![],
-        }
-    }
-}
-
-#[derive(Debug, InputAction)]
-#[action_output(Vec3)]
-struct Move;
-
-#[derive(Debug, InputAction)]
-#[action_output(Vec2)]
-struct Look;
